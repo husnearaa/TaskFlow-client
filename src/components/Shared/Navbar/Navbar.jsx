8238bbefbd95c9f0422e4f73eb095219469ab1b9
husneara <husnearaa99@gmail.com>
2023-12-24 23:44:45 +0600

    has updated login user profile

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../../provider/AuthProvider";
+import Swal from "sweetalert2";
 
 
 
@@ -12,10 +13,32 @@ const Navbar = () => {
     const { user, logOut } = useContext(AuthContext);
 
 
+    // const handleLogOut = () => {
+    //     logOut()
+    //         .then(() => { })
+    //         .catch(error => console.log(error));
+    // }
+
     const handleLogOut = () => {
-        logOut()
-            .then(() => { })
-            .catch(error => console.log(error));
+        Swal.fire({
+            title: "Are you sure to log out?",
+            text: "Once you logout you have to login again",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Log Out!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                logOut()
+                    .then(res => console.log(res))
+                Swal.fire({
+                    title: "Logged Out!",
+                    text: "Your Account logged Out Successfully",
+                    icon: "success"
+                });
+            }
+        });
     }
 
 
@@ -73,16 +96,26 @@ const Navbar = () => {
                 {/* <Link to={"/login"}><button className="btn border-[#fed700] text-black bg-[#fed700] px-10 rounded-full">Login</button>
             </Link> */}
 
-                {
-                    user ? <>
-                        {/* {user?.displayName} */}
-                        <span className="text-white">{user?.displayName}</span>
-                        <button onClick={handleLogOut} className="btn border-[#d88531] text-black bg-[#d88531] px-10 rounded-md">Logout</button>
-                    </> : <>
-                        <Link to="/login"><button className="btn border-[#d88531] text-black bg-[#d88531] px-10 rounded-md">Login</button></Link>
-
-                    </>
-                }
+                <div>
+                    {
+                        user ?
+                            <>
+                                <div className="flex justify-between items-center ">
+                                    <img className='rounded-full w-[35px] md:w-[40px] h-[35px] md:h-[40px]' src={user?.photoURL} alt='' />
+                                    <h1 className="text-white w-2/3 pr-1">{user?.displayName}</h1>
+                                    <button onClick={handleLogOut} className="btn border-[#d88531] text-white bg-transparent px-10 rounded-md">
+                                        Logout
+                                    </button>
+                                </div>
+                            </>
+                            :
+                            <NavLink to='/login'>
+                                <li className="btn border-[#d88531] text-white bg-transparent px-10 rounded-md">
+                                    <a>Log in</a>
+                                </li>
+                            </NavLink>
+                    }
+                </div>
             </nav>
         </div>
     );
